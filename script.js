226267a6c0c757119aec7c1ed66e17745c910d55
MichaPotoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-08-26 11:53:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function isRectangularTriangle(x1, x2, x3) {
+  
+  const nrs = [x1, x2, x3];
+ 
+  const minimum = Math.min(...nrs);
+
+  console.log(minimum)
+  //Math.pow(liczba1, liczba2)
+}
+
+isRectangularTriangle(8,5,6);
+
+/*const cond1 = isRectangularTriangle(3, 4, 5);
+const cond2 = isRectangularTriangle(4, 3, 5);
+// cond1 i cond2 to true
+
+const cond3 = isRectangularTriangle(4, 3, 2);
+const cond4 = isRectangularTriangle(4, 4, 4);
+// cond3 i cond4 to false*/
+
+
+
+
